2f8a95d8dc2e023b4b3e8b5d56e0345dc0e9cc6f
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-09-14 13:37:36 -0700

    more realistic model temp and discharge

--- jags-normal-linear.R
+++ jags-normal-linear.R
@@ -1,17 +1,18 @@
-### Simulate river velocity in relation to discharge
+### Simulate river temperature in relation to discharge
 library(jmbr)
 library(ggplot2)
 
 discharge <- runif(100, 0, 50)
-bDischarge <- 2.4
-bIntercept <- 5
-bSigma <- 4
-x <- bIntercept + bDischarge*discharge
-velocity <- rnorm(100, mean = x, sd = bSigma)
+bDischarge <- -0.2
+bIntercept <- 25
+bSigma <- 2
+x <- bIntercept + bDischarge*discharge 
+temperature <- rnorm(100, mean = x, sd = bSigma)
 
 data <- data.frame(Discharge = discharge,
-                   Velocity = velocity)
-ggplot(data = data, aes(x = Discharge, y = Velocity)) +
+                   Temperature = temperature)
+
+ggplot(data = data, aes(x = Discharge, y = Temperature)) +
   geom_point()
 
 model <- model("model {
@@ -21,23 +22,23 @@ model <- model("model {
   bSigma ~ dnorm(4, 10^-2) T(0,)
   
   for(i in 1:nObs) {
-    eVelocity[i] <- bIntercept + bDischarge * Discharge[i]  
-    Velocity[i] ~ dnorm(eVelocity[i], bSigma^-2)
+    eTemperature[i] <- bIntercept + bDischarge * Discharge[i]  
+    Temperature[i] ~ dnorm(eTemperature[i], bSigma^-2)
   }
 }", new_expr = "
   for(i in 1:nObs) {
-    eVelocity[i] <- bIntercept + bDischarge * Discharge[i]  
-    prediction[i] <- eVelocity[i]
+    eTemperature[i] <- bIntercept + bDischarge * Discharge[i]  
+    prediction[i] <- eTemperature[i]
   }
 ", nthin = 10L, select_data = list(Discharge = c(0, 1000),
-                                   Velocity = c(-100, 10000)))
+                                   Temperature = c(-10, 40)))
 
 analysis <- analyse(model, data = data)
 coef(analysis, simplify = TRUE)
 prediction <- predict(analysis, new_data = c("Discharge")) 
 
 gp <- ggplot(data = prediction, aes(x = Discharge, y = estimate)) +
-  geom_point(data = data, aes(y = Velocity)) +
+  geom_point(data = data, aes(y = Temperature)) +
   geom_line() +
   geom_line(aes(y = lower), linetype = "dotted") +
   geom_line(aes(y = upper), linetype = "dotted") +
