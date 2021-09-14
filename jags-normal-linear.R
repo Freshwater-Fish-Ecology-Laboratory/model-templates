8f628f2f550c1b953da9fb8c91b54e7d00a16683
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-09-14 12:55:57 -0700

    simlate discharge/velocity model

---
+++ jags-normal-linear.R
@@ -0,0 +1,46 @@
+### Simulate river velocity in relation to discharge
+library(jmbr)
+library(ggplot2)
+
+discharge <- runif(100, 0, 50)
+bDischarge <- 2.4
+bIntercept <- 5
+bSigma <- 4
+x <- bIntercept + bDischarge*discharge
+velocity <- rnorm(100, mean = x, sd = bSigma)
+
+data <- data.frame(Discharge = discharge,
+                   Velocity = velocity)
+ggplot(data = data, aes(x = Discharge, y = Velocity)) +
+  geom_point()
+
+model <- model("model {
+
+  bIntercept ~ dnorm(0, 5^-2)
+  bDischarge ~ dnorm(0, 5^-2)
+  bSigma ~ dnorm(4, 10^-2) T(0,)
+  
+  for(i in 1:nObs) {
+    eVelocity[i] <- bIntercept + bDischarge * Discharge[i]  
+    Velocity[i] ~ dnorm(eVelocity[i], bSigma^-2)
+  }
+}", new_expr = "
+  for(i in 1:nObs) {
+    eVelocity[i] <- bIntercept + bDischarge * Discharge[i]  
+    prediction[i] <- eVelocity[i]
+  }
+", nthin = 10L, select_data = list(Discharge = c(0, 1000),
+                                   Velocity = c(-100, 10000)))
+
+analysis <- analyse(model, data = data)
+coef(analysis, simplify = TRUE)
+prediction <- predict(analysis, new_data = c("Discharge")) 
+
+gp <- ggplot(data = prediction, aes(x = Discharge, y = estimate)) +
+  geom_point(data = data, aes(y = Velocity)) +
+  geom_line() +
+  geom_line(aes(y = lower), linetype = "dotted") +
+  geom_line(aes(y = upper), linetype = "dotted") +
+  NULL
+print(gp)
+
