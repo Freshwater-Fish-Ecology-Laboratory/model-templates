1eea9bb4389ce882512056fb71fd653af56e90ea
Seb Dalgarno <seb.dalgarno@gmail.com>
2021-09-14 10:07:14 -0700

    basic logistic linear model

---
+++ jags-logistic-linear.R
@@ -0,0 +1,48 @@
+### simulate data
+library(jmbr)
+library(ggplot2)
+
+distance <- runif(100, 0, 1000)
+bDistance <- -0.02
+bIntercept <- 4
+x <- bIntercept + bDistance*distance
+p <- 1/(1 + exp(-x))
+pings <- round(runif(100, 30, 70))
+detects <- rbinom(100, size = pings, p = p)
+
+data <- data.frame(Distance = distance, 
+                   Pings = as.integer(pings), 
+                   Detects = as.integer(detects))
+ggplot(data = data, aes(x = Distance, y = Detects/Pings)) +
+  geom_point()
+
+model <- model("model {
+
+  bIntercept ~ dnorm(0, 5^-2)
+  bDistance ~ dnorm(0, 5^-2)
+  
+  for(i in 1:nObs) {
+    logit(eDetects[i]) <- bIntercept + bDistance * Distance[i]  
+    Detects[i] ~ dbin(eDetects[i], Pings[i])
+  }
+}", new_expr = "
+  for(i in 1:nObs) {
+    logit(eDetects[i]) <- bIntercept + bDistance * Distance[i]  
+    prediction[i] <- eDetects[i]
+  }
+", nthin = 10L, select_data = list(Detects = c(0L, 10000L), 
+                                   Distance = c(0, 1000),
+                                   Pings = c(1L, 10000L)))
+
+analysis <- analyse(model, data = data)
+coef(analysis, simplify = TRUE)
+prediction <- predict(analysis, new_data = c("Distance")) 
+
+gp <- ggplot(data = prediction, aes(x = Distance, y = estimate)) +
+  geom_point(data = data, aes(y = Detects/Pings)) +
+  geom_line() +
+  geom_line(aes(y = lower), linetype = "dotted") +
+  geom_line(aes(y = upper), linetype = "dotted") +
+  NULL
+print(gp)
+
